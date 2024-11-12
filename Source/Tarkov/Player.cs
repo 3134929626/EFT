deb35f12c096aa0b8b3b3b2a5d992a80b87d8316
BanZ1 <LocalEmail@qq.com>
2024-11-12 13:09:49 +0800

    去除万圣节僵尸判断

--- Source/Tarkov/Player.cs
+++ Source/Tarkov/Player.cs
@@ -947,8 +947,7 @@ namespace eft_dma_radar
             foreach (var bone in RequiredBones)
             {
                 var boneIndex = (uint)bone;
-                if (this.Name == "???")
-                    boneIndex = (uint)31;
+
                 var pointer = Memory.ReadPtrChain(boneMatrix, [0x20 + (boneIndex * 0x8), 0x10]);
                 //var Fire = Memory.ReadPtrChain(this.Base, [Offsets.Player.ProceduralWeaponAnimation, 0x18, 0x80, 0x10]);
 
